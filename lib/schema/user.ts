97196acb466e46868573eebe3c4dcc4ce5e086f0
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-07-27 10:27:12 +0700

    mod user schema

--- lib/schema/user.ts
+++ lib/schema/user.ts
@@ -1,11 +1,29 @@
 import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
   {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, 'Please enter a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your email'],
+      minLength: [6, 'Your password must be at least 6 characters long'],
+      select: false, //dont send back password after request
+    },
+    role: {
+      type: String,
+      default: 'user',
+      enum: {
+        values: ['user', 'admin'],
+      },
+    },
     name: String,
-    email: String,
-    password: String,
-    role: String,
     status: String,
     phoneNumber: Number,
     address: String,
@@ -15,6 +33,19 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+// ENCRYPTION
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password as string, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
