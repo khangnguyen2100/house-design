f7058694ac5f8663c77789f805671a4de9d752c0
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-07-27 10:26:53 +0700

    enhance connect db

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -1,18 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose, { ConnectOptions } from 'mongoose';
 
-const connectDb = async () => {
-  try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error(
-        'Connect to MongoDB failed. Check your environment variables.',
-      );
-    }
-    if (!mongoose.connections[0].readyState) {
-      await mongoose.connect(process.env.MONGODB_URI);
-    }
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.log('error:', error);
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+
+const global: any = {};
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDb() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(
+        mongoUri as string,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as ConnectOptions,
+      )
+      .then(mongoose => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 export default connectDb;
